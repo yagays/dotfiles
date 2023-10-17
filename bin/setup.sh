d6bd2f0fbe45ade7291cf7ae6b25d7574a9c59d6
Yuki Okuda <y.okuda@dr-ubie.com>
2023-10-17 10:14:41 +0900

    update

---
+++ bin/setup.sh
@@ -0,0 +1,11 @@
+# remove all .localized files from home directory
+rm ~/Applications/.localized
+rm ~/Documents/.localized
+rm ~/Downloads/.localized
+rm ~/Desktop/.localized
+rm ~/Public/.localized
+rm ~/Pictures/.localized
+rm ~/Music/.localized
+rm ~/Movies/.localized
+rm ~/Library/.localized
+rm /Applications/.localized

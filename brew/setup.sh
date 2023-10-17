e151fafee439e14d85810593badd2a09cb9f6097
Yuki Okuda <y.okuda@dr-ubie.com>
2023-10-17 12:34:52 +0900

    update

--- brew/setup.sh
+++ brew/setup.sh
@@ -1,5 +1,2 @@
 # for macOS
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# for install google-japanese-ime
-sudo softwareupdate --install-rosetta
---
+++ install.sh
@@ -0,0 +1,5 @@
+# for installing google-japanese-ime
+sudo softwareupdate --install-rosetta
+
+# for continuous key repeat
+sudo defaults write -g ApplePressAndHoldEnabled -bool true

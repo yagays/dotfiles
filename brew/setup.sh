6a89e9afe50a4ce310ce6cbdb09865eae29213b5
Yuki Okuda <y.okuda@dr-ubie.com>
2023-10-17 10:24:56 +0900

    update

--- brew/setup.sh
+++ brew/setup.sh
@@ -1,2 +1,5 @@
 # for macOS
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# for install google-japanese-ime
+sudo softwareupdate --install-rosetta
